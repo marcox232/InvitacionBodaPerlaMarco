2cfaf9d103e07b4aa5f1bcfde6adf74599d33c4b
marcox232 <rodriguez.bizarro232@gmail.com>
2023-10-28 02:17:44 -0600

    Add files via upload
    Se carga la rama principal que se trabajo con para la Invitacion Digital, se realiza la primera carga

---
+++ javascript.js
@@ -0,0 +1,127 @@
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+
+    
+    //===
+    // VARIABLES
+    //===
+    const DATE_TARGET = new Date('12/09/2023 03:30 PM');
+    // DOM for render
+    const SPAN_DAYS = document.querySelector('span#days');
+    const SPAN_HOURS = document.querySelector('span#hours');
+    const SPAN_MINUTES = document.querySelector('span#minutes');
+    const SPAN_SECONDS = document.querySelector('span#seconds');
+    // Milliseconds for the calculations
+    const MILLISECONDS_OF_A_SECOND = 1000;
+    const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+    const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+    const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+    //===
+    // FUNCTIONS
+    //===
+
+    /**
+    * Method that updates the countdown and the sample
+    */
+    function updateCountdown() {
+        // Calcs
+        const NOW = new Date()
+        const DURATION = DATE_TARGET - NOW;
+        const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+        const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+        const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+        const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+        // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
+
+        // Render
+        SPAN_DAYS.textContent = REMAINING_DAYS;
+        SPAN_HOURS.textContent = REMAINING_HOURS;
+        SPAN_MINUTES.textContent = REMAINING_MINUTES;
+        SPAN_SECONDS.textContent = REMAINING_SECONDS;
+    }
+
+    //===
+    // INIT
+    //===
+    updateCountdown();
+    // Refresh every second
+    setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+});
+
+//==
+//Java Script Galeria
+//==
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+    moveToRight()
+}, 30000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}   
+
+
+//Confirmacion de Invitados
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzVv1lKGiKNX2QtuTlbyRmBfLFsXzK6bd0KAxyXZ_0_bPvIV1hc-KKn5VeyZq4SSmVM/exec'
+const form = document.forms['submit-to-google-sheet']
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+      document.getElementById("formulario-invitados").reset();
+      var x = document.getElementById("informacion-invitados-confirmacion");
+      var segundoDiv = document.getElementById("mensaje-confirmacion-invitados");
+      if (x.style.display === "none") {
+          x.style.display = "block";
+          segundoDiv.style.display = "none";
+      } else {
+          x.style.display = "none";
+          segundoDiv.style.display = "block";
+      }  
+  }
+  )
